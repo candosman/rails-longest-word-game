ee282b94b1e06c4f96b7e39e9377646dc1fc2cee
Bryan Chuttoo <chuttoobryan04@gmail.com>
2024-05-14 04:20:37 +0400

    done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,25 @@
+require "open-uri"
 class GamesController < ApplicationController
   def new
     @letters = ("a".."z").to_a.shuffle.first(10)
-
   end
+
   def score
-    @word = params[:word]
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
 
+  private
 
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://dictionary.lewagon.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
